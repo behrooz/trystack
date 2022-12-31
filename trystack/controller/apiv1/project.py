19d7dde16653c3de7fbd7fc7220bf107a3a7caa9
behrooz <razzaghi.b@gmail.com>
2022-12-31 19:35:25 +0330

    add update and delete restapi

--- trystack/controller/apiv1/project.py
+++ trystack/controller/apiv1/project.py
@@ -1,5 +1,6 @@
 from flask import request
 
+from trystack.trystack import db
 from trystack.decorator import json_required
 from trystack.model import Project
 from trystack.util import jsonify
@@ -16,14 +17,51 @@ class ProjectController():
 
     @json_required
     def get_project(project_id):
-        return jsonify(status=501)        
+        project = Project.query.get(project_id)
+        if project is None:
+            return jsonify(status=404)
+        project_schema = ProjectSchema()
+        return jsonify(
+            {"project":project_schema.dump(project)}
+        )
+
     @json_required
     def create_project():
-        return jsonify(status=501)
+        project_schema = ProjectSchema(only=["name"])        
+        request_data = project_schema.load(request.get_json())
+        project = Project.query.filter_by(name=request_data["name"]).first()
+        if project is not None:
+            return jsonify(status=409)
+        project = Project(name=request_data["name"])            
+        db.session.add(project)
+        db.session.commit()
+        project_schema = ProjectSchema()
+        return jsonify(
+            state={"project": project_schema.dump(project)},
+            status=201
+        )
+
+
     @json_required
-    def update_project(project_id):
-        return jsonify(status=501)
+    def update_project(project_id):        
+        project_schema = ProjectSchema(only=["status"])
+        request_data = project_schema.load(request.get_json())
+        project = Project.query.get(project_id)        
+        if project is None:
+            return jsonify(status=404)
+        project.status = request_data["status"]
+        db.session.commit()
+        project_schema = ProjectSchema()
+        return jsonify(
+            state=project_schema.dump(project),            
+        )
+
     @json_required
     def delete_project(project_id):
-        return jsonify(status=501)
+        project = Project.query.get(project_id)
+        if project is None:
+            return jsonify(status=404)
+        db.session.delete(project)    
+        db.session.commit()
+        return jsonify(status=204)
         
