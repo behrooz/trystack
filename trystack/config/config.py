3f4cd93a9e6b77918ffab63a505c0b6297b2c240
behrooz <razzaghi.b@gmail.com>
2022-12-31 14:12:10 +0330

    add util folder and configs

--- trystack/config/config.py
+++ trystack/config/config.py
@@ -6,3 +6,8 @@ class Config:
      ENV = environ.get("TRYSTACK_API_ENV","production")
      DEBUG = environ.get("TRYSTACK_API_DEBUG", "0")
      TESTING = DEBUG
+
+     SQLALCHEMY_DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI","mysql+pymysql://root:123@192.168.2.65:3306/trystack")
+     SQLALCHEMY_RECORD_MODIFICATIONS = DEBUG
+     SQLALCHEMY_RECORD_QUERIES = DEBUG
+     SQLALCHEMY_ECHO = DEBUG
