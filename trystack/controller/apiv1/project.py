f692f835734c2b36df195ca2311f3bf60dd25635
behrooz <razzaghi.b@gmail.com>
2022-12-31 18:38:32 +0330

    add schema to controller

--- trystack/controller/apiv1/project.py
+++ trystack/controller/apiv1/project.py
@@ -1,19 +1,29 @@
-from trystack.model import project
+from flask import request
+
+from trystack.decorator import json_required
+from trystack.model import Project
 from trystack.util import jsonify
+from trystack.scehma.apiv1 import ProjectSchema
 
 class ProjectController():
+    @json_required
     def get_projects():
-        return jsonify(status=501)
+        projects = Project.query.all()
+        projects_schema = ProjectSchema(many=True)
+        return jsonify(
+            {"projects": projects_schema.dump(projects)}
+        )
 
+    @json_required
     def get_project(project_id):
         return jsonify(status=501)        
-
+    @json_required
     def create_project():
         return jsonify(status=501)
-
+    @json_required
     def update_project(project_id):
         return jsonify(status=501)
-
+    @json_required
     def delete_project(project_id):
         return jsonify(status=501)
         
