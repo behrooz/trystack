80dc536ae933c14da46cb0f70654daa558f9743c
behrooz <razzaghi.b@gmail.com>
2023-02-13 12:50:00 +0330

    create product tbl and migration

--- trystack/model/__init__.py
+++ trystack/model/__init__.py
@@ -1,3 +1,5 @@
 from .project import Project
 from .user import User
 from .category import Category
+from .productcategory import ProjectCategory
+from .product import Product
--- trystack/model/productcategory.py
+++ trystack/model/productcategory.py
@@ -4,5 +4,4 @@ from trystack.util import uuidgen,now
 class ProjectCategory(db.Model):
 
     id=db.Column(db.String(64), primary_key=True, default=uuidgen)
-    category_id=db.Column(db.ForeignKey("category.id"))        
-    #parent= db.relationship("Category", backref="children")                
+    category_id=db.Column(db.ForeignKey("category.id"))    
