05af7fc6b73a7f0208ba0f6addceac6915a528fd
behrooz <razzaghi.b@gmail.com>
2023-02-13 12:58:26 +0330

    create product tbl and category relationship migration

---
+++ trystack/model/product.py
@@ -0,0 +1,19 @@
+from trystack.trystack import db
+from trystack.util import uuidgen, now
+
+class Product(db.Model):
+
+    id=db.Column(db.String(64), primary_key=True,default=uuidgen)
+    userId=db.Column(db.String(64),nullable=False)
+    title=db.Column(db.String(64),nullable=False)
+    metaTile=db.Column(db.String(64),nullable=True)
+    slug=db.Column(db.String(64),nullable=False)
+    summary=db.Column(db.Text,nullable=False)    
+    price=db.Column(db.String(64),nullable=False)
+    discount=db.Column(db.String(64),nullable=False)
+    quantity=db.Column(db.String(64),nullable=False)    
+    createdAt=db.Column(db.DateTime,nullable=True,default=now)
+    updateAt=db.Column(db.DateTime,nullable=True)
+    publishedAt=db.Column(db.DateTime,nullable=True)
+    content=db.Column(db.Text,nullable=True)
+    products=db.relationship("ProjectCategory",backref="product")
